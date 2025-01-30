7099503174f30318d45399ca11f3244697453baa
professor steve h <steve@use.my.school.email.addr>
2025-01-30 17:07:39 -0500

    week04 inclass work and week05 slides material

---
+++ Week04/inclass/q2/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "utils.h"
+
+// write a program to determine the max of 3 ints
+// store the largest in a variable called max
+
+//steveh - prog71990w25 - week4
+
+int main(void) {
+
+	//input
+	int a = 1;
+	int b = 3;
+	int c = 2;
+
+	//transform/calculate/do stuff
+	int max = get_max_of_3_ints(a,b,c);
+		
+	//output
+	printf("the max of %d, %d, %d is %d\n", a,b,c, max); 
+	return 0;
+}
+
+int get_max_of_3_ints(int a, int b, int c) {
+	int max = a;
+	if (b > max)
+		max = b;
+	if (c > max)
+		max = c;
+
+	return max;
+}
---
+++ Week04/inclass/q2/utils.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//  this is library of random functions
+
+// steveh - prog71990w25 - week04
+
+int get_max_of_3_ints(int a, int b, int c);	//formal prototype
+											//function signature
+
+
